e645873247d4c5e4140e9d859c68ffc3a156baf0
sai <sai@DESKTOP-1181R47>
2026-03-30 16:51:46 +0530

    Made changes

--- sailogin/login.aspx.cs
+++ sailogin/login.aspx.cs
@@ -13,7 +13,19 @@ namespace sailogin
         SqlConnection conn = new SqlConnection("Data Source=DESKTOP-1181R47\\SQLEXPRESS;Initial Catalog=sai;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                bindgrid();
+            }
 
+        }
+        protected void bindgrid()
+        {
+            SqlCommand cmd = new SqlCommand("select*from login", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            GridView.DataSource = dr;
+            GridView.DataBind();
         }
 
         protected void btnsubmit_Click(object sender, EventArgs e)
@@ -24,6 +36,37 @@ namespace sailogin
             conn.Open();
             cmd.ExecuteNonQuery();
             conn.Close();
+            bindgrid();
+        }
+
+        protected void GridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView.DataKeys[e.RowIndex].Value);
+            SqlCommand cmd = new SqlCommand("delete from login where userid=@userid", conn);
+            cmd.Parameters.AddWithValue("@userid", userid);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            bindgrid();
+        }
+        protected void GridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewRow row = GridView.Rows[e.NewEditIndex];
+            ViewState["userid"] = GridView.DataKeys[e.NewEditIndex].Value;
+            txtuname.Text = row.Cells[2].Text;
+            txtpassword.Text = row.Cells[3].Text;
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("update login set username=@username,password=@password where userid=@userid",conn);
+            cmd.Parameters.AddWithValue("@userid", ViewState["userid"]);
+            cmd.Parameters.AddWithValue("@username",txtuname.Text);
+            cmd.Parameters.AddWithValue("@password",txtpassword.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            bindgrid();
         }
     }
 }
--- sailogin/login.aspx.designer.cs
+++ sailogin/login.aspx.designer.cs
@@ -67,5 +67,23 @@ namespace sailogin
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnsubmit;
+
+        /// <summary>
+        /// btnupdate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnupdate;
+
+        /// <summary>
+        /// GridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView;
     }
 }
