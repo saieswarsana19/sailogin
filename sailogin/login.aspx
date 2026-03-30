98e70cfc894c50d4ae585726a27cb9b6e6df6a73
sai <sai@DESKTOP-1181R47>
2026-03-30 15:46:50 +0530

    Add project files.

---
+++ sailogin/login.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="sailogin.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbluname" runat="server" Text="username"></asp:Label><br />
+            <asp:TextBox ID="txtuname" runat="server"></asp:TextBox><br />
+            <asp:Label ID="lblpassword" runat="server" Text="password"></asp:Label><br />
+            <asp:TextBox ID="txtpassword" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnsubmit" runat="server" OnClick="btnsubmit_Click" Text="submit" /><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ sailogin/login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace sailogin
+{
+    public partial class login : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-1181R47\\SQLEXPRESS;Initial Catalog=sai;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into login(username,password)values(@username,@password)", conn);
+            cmd.Parameters.AddWithValue("@username",txtuname.Text);
+            cmd.Parameters.AddWithValue("@password",txtpassword.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
